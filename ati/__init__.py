7d0a34e9228d2d5dc18236b36381b28f90082ee0
˶𝞢⤬⫒ⵖsᐼ˶ <91800957+exdysa@users.noreply.github.com>
2026-06-11 18:05:28 -0400

    +MOAR params

--- ati/__init__.py
+++ ati/__init__.py
@@ -10,27 +10,38 @@ class ATISettings(pyds.BaseSettings):
     steps: int
     dtype: str | Any
     seed: int | None = 0
-    learning_rate: float | None = None
     cooldown: int | None = None
-    warmup: int | None = None
     epochs: int | None = None
+    learning_rate: float | None = None
+    warmup: int | None = None
 
     # Dimensions
+    attention_head_dimension: int | None = None
+    axes_dimension: list | None = None
     batch_size: int | None = None
     condition_dimension: int | None = None
+    context_in_dimension: int | None = None
     head_dimension: int | None = None
+    height: int | None = None
     hidden_dimension: int | None = None
+    in_channels: int | None = None
     input_dimension: int | None = None
     patch_size_2d: tuple[int, int] | None = None
     patch_size: int | None = None
     resolution: int | None = None
+    speaker_embedding_dimension: int | None = None
+    text_embedding_dimension: int | None = None
     time_dimension: int | None = None
+    vector_in_dimension: int | None = None
+    width: int | None = None
 
     # Layers
+    depth: int | None = None
     iterations: int | None = None
     layers: int | None = None
     mlp_activation: str | None = None
     mlp_hidden_layers: int | None = None
+    depth_single_blocks: int | None = None
 
     # Paths
     checkpoint_path: str | None = None
@@ -41,7 +52,7 @@ class ATISettings(pyds.BaseSettings):
 
     # Factoring
     condense_factor: int | None = None
-    dim_factor: int | None = None
+    dimension_factor: int | None = None
 
     # Datasets
     column: str | None = None
@@ -52,46 +63,55 @@ class ATISettings(pyds.BaseSettings):
     # XGB
     alpha: float | None = None
     colsample_bytree: float | None = None
+    d_matrix_test: Any | None = None
     early_stopping_rounds: int | None = None
+    feature_matrix: Any | None = None
+    max_depth: int | None = None
     max_rnd: int | None = None
     n_components: float | None = None
     num_boost_round: int | None = None
+    objective: str | None = None
+    pca: Any | None = None
     subsample: float | None = None
     test_size: float | None = None
     train_rounds: int | None = None
-    d_matrix_test: Any | None = None
-    feature_matrix: Any | None = None
-    pca: Any | None = None
     X_train_pca: Any | None = None
-    max_depth: int | None = None
-    objective: str | None = None
 
     # Evaluation
     eval_metric: list | None = None
     verbose_eval: int | None = None
 
     # Inference
+    agent_prompt: str | None = None
     inference_batch_size: int | None = None
-    inference_steps: int | None = None
     inference_cfg: float | None = None
+    inference_steps: int | None = None
+    negative_prompt: str | None = None
+    positive_prompt: str | None = None
     prompt: str | None = None
     system_prompt: str | None = None
-    positive_prompt: str | None = None
-    negative_prompt: str | None = None
     wildcard_prompt: str | None = None
-    agent_prompt: str | None = None
 
     # misc toggle
     disable_nullable: bool | None = None
+    guidance_embed: bool | None = None
     jit_type: bool | None = None
     load_from_cache_file: bool | None = None
     load_onnx: bool | None = None
     magnitude_sampling: bool | None = None
+    qkv_bias: bool | None = None
+    shift: bool | None = None
 
     # misc int
+    block_length: int | None = None
     cv: int | None = None
     gradient_accumulation: int | None = None
+    mask_id: int | None = None
+    max_position_embeddings: int | None = None
+    max_position: int | None = None
+    max_text_length: int | None = None
     mlp_max_iter: int | None = None
+    num_attention_heads: int | None = None
     num_features: int | None = None
     number_folds: int | None = None
     svm_c: int | None = None
@@ -100,22 +120,26 @@ class ATISettings(pyds.BaseSettings):
 
     # misc float
     abstain_threshold: float | None = None
+    cfg: float | None = None
+    mlp_ratio: float | None = None
+    rope_theta: float | None = None
     scale_pos_weight: float | None = None
     t_eps: float | None = None
-    cfg: float | None = None
 
     # misc string
+    cfg_type: str | None = None
     gamma: str | None = None
+    guider: str | None = None
     kernel: str | None = None
     method: str | None = None
-    guider: str | None = None
+    remasking_strategy: str | None = None
+    scheduler_type: str | None = None
 
     # misc
+    additional_kwargs: dict[str, Any] | None = None
+    attention_type: Any | None = None
     labels: Any | None = None
     model_type: Any | None = None
-    attention_type: Any | None = None
-
-    additional_kwargs: dict[str, Any] | None = None
 
 
 def main(**kwargs):
